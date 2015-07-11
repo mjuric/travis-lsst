c592cf79591f4a17b64b8d11b7d2e5d45271e48a
Mario Juric <mjuric@lsst.org>
2015-07-11 00:14:34 -0700

    Cache only the database cache dir

--- prepare.sh
+++ prepare.sh
@@ -6,20 +6,21 @@
 
 set -e
 
-if [[ ! -f conda-lsst/.deployed ]]; then
-	# Prepare conda-lsst
-	rm -rf conda-lsst
-	git clone https://github.com/mjuric/conda-lsst
-	cd conda-lsst
-	./bin/bootstrap.sh
-	export PATH="$PWD/bin:$PWD/miniconda/bin:$PATH"
-	hash -r
-	conda config --set always_yes yes --set changeps1 no
-	conda config --add channels http://eupsforge.net/conda/dev
-	touch .deployed
-else
-	# Already here, from Travis' cache
-	cd conda-lsst
-	export PATH="$PWD/bin:$PWD/miniconda/bin:$PATH"
-	hash -r
-fi
+# keep the pkginfo cache database outside the conda-lsst dir
+# so Travis can cache it easily.
+mkdir -p pkginfo-cache
+
+# Prepare conda-lsst environment
+rm -rf conda-lsst
+git clone https://github.com/mjuric/conda-lsst
+cd conda-lsst
+./bin/bootstrap.sh
+export PATH="$PWD/bin:$PWD/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels http://eupsforge.net/conda/dev
+
+# symlink the database dir
+ln -s ../pkginfo-cache
+ls -l
+ls -lRt pkginfo-cache
